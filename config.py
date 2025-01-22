5a0071b1dfa71fc24f718c7d4f387da119332e06
OmarVB77 <omar.vega.b@uni.pe>
2025-01-22 16:11:21 -0500

    Segundo api

---
+++ config.py
@@ -0,0 +1,13 @@
+MYSQL_CONFIG = {
+    'host': '127.0.0.1:3306',
+    'user': 'u777467137_equifax',
+    'password': 'Logaritmo10+',
+    'database': 'u777467137_equifax'
+}
+
+SQLSERVER_CONFIG = {
+    'host': 'localhost',
+    'database': 'Northwind',
+    'driver': 'ODBC Driver 17 for SQL Server',
+    'trusted_connection': 'yes'
+}
